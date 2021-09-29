6129aa94afa4f39b1a7962d277859ad4c127eaf6
NFRas <nrasoulipour@gmail.com>
2021-09-29 09:20:43 +0330

    creatingt the profile function

--- 2nd-Bootstrap-demo-template/userinfo.js
+++ 2nd-Bootstrap-demo-template/userinfo.js
@@ -1,9 +1,16 @@
 //in this page i shoudl use the param query stuff
 //this page opens after clicking every user
 
+let user = getElement
   
-  async function userinfo() {
-   console.log(mojombo);
+  async function userinfo(x) {
+   let url = `https://api.github.com/users/?login=${user}`;
+   try {
+       let profile = await fetch(url);
+       return await profile.json();
+   } catch (err) {
+   console.log(err);
+   }
   }
   
   userinfo();
